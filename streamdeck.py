51a1c9488576cbd05519a86cb6086901647544ec
Erik Finskas <erik.finskas@gmail.com>
2025-01-12 19:15:06 +0200

    fix main

--- streamdeck.py
+++ streamdeck.py
@@ -22,18 +22,18 @@ long_press_ack_keys = set()  # Track keys that are in long press acknowledgment 
 # Flag for stopping the main loop
 stop_flag = threading.Event()
 
-def create_image(size, text, style):
+def create_image(size, text, style, font):
     image = Image.new('RGB', size, style['bg_color'])
     draw = ImageDraw.Draw(image)
     
     # Calculate text position, centered
-    text_bbox = draw.textbbox((0, 0), text, font=style['font'])
+    text_bbox = draw.textbbox((0, 0), text, font=font)
     text_width = text_bbox[2] - text_bbox[0]
     text_height = text_bbox[3] - text_bbox[1]
     text_position = ((size[0] - text_width) // 2, (size[1] - text_height) // 2)
     
     # Draw the text on the image
-    draw.text(text_position, text, font=style['font'], fill=style['text_color'])
+    draw.text(text_position, text, font=font, fill=style['text_color'])
     
     image = ImageOps.mirror(image)  # Flip the image horizontally to fix mirrored text
     
@@ -41,44 +41,50 @@ def create_image(size, text, style):
     image.save(image_bytes, format='BMP')
     return image_bytes.getvalue()
 
-def create_highlighted_image(size, text, style):
+def create_highlighted_image(size, text, style, font):
     return create_image(size, text, {
         'bg_color': style['highlight_bg_color'],
         'text_color': style['highlight_text_color'],
-        'font': style['font'],
-    })
+    }, font)
 
 def load_configuration():
-    global styles, button_config, parameters
+    global styles, button_config, parameters, font
 
     # Connect to the SQLite database
     conn = sqlite3.connect('streamdeck.db')
     cursor = conn.cursor()
 
     # Fetch styles
-    cursor.execute('SELECT name, bg_color, text_color, font_path, font_size, highlight_bg_color, highlight_text_color FROM styles')
-    styles = {row[0]: {
-        'bg_color': row[1],
-        'text_color': row[2],
-        'font': ImageFont.truetype(row[3], row[4]),
+    cursor.execute('SELECT id, device_id, name, bg_color, text_color, highlight_bg_color, highlight_text_color, `default` FROM styles')
+    styles = {row[2]: {
+        'device_id': row[1],
+        'bg_color': row[3],
+        'text_color': row[4],
         'highlight_bg_color': row[5],
-        'highlight_text_color': row[6]
+        'highlight_text_color': row[6],
+        'default': row[7]
     } for row in cursor.fetchall()}
 
     # Fetch button configurations
-    cursor.execute('SELECT key, text, style, long_press_ack_style, short_press, long_press, ack_action FROM button_config')
-    button_config = {row[0]: {
-        'text': row[1],
-        'style': row[2],
-        'long_press_ack_style': row[3],
-        'short_press': row[4],
-        'long_press': row[5],
-        'ack_action': row[6]
+    cursor.execute('SELECT device_id, key, text, style, long_press_ack_style, short_press, long_press, ack_action FROM button_config')
+    button_config = {row[1]: {
+        'device_id': row[0],
+        'text': row[2],
+        'style': row[3],
+        'long_press_ack_style': row[4],
+        'short_press': row[5],
+        'long_press': row[6],
+        'ack_action': row[7]
     } for row in cursor.fetchall()}
 
     # Fetch parameters
     cursor.execute('SELECT name, value FROM parameters')
     parameters = {row[0]: row[1] for row in cursor.fetchall()}
+
+    # Load font
+    font_path = parameters['font_path']
+    font_size = int(parameters['font_size'])
+    font = ImageFont.truetype(font_path, font_size)
 
     # Close the database connection
     conn.close()
@@ -87,9 +93,9 @@ def load_configuration():
     for key in range(deck.key_count()):
         style_name = button_config[key]['style']
         style = styles[style_name]
-        original_image = create_image(deck.key_image_format()['size'], button_config[key]['text'], style)
-        highlighted_image = create_highlighted_image(deck.key_image_format()['size'], button_config[key]['text'], style)
-        long_press_ack_image = create_image(deck.key_image_format()['size'], button_config[key]['text'], styles['long_press_ack'])
+        original_image = create_image(deck.key_image_format()['size'], button_config[key]['text'], style, font)
+        highlighted_image = create_highlighted_image(deck.key_image_format()['size'], button_config[key]['text'], style, font)
+        long_press_ack_image = create_image(deck.key_image_format()['size'], button_config[key]['text'], styles['long_press_ack'], font)
         images[key] = {
             'original': original_image,
             'highlighted': highlighted_image,
@@ -141,9 +147,9 @@ try:
     for key in range(deck.key_count()):
         style_name = button_config[key]['style']
         style = styles[style_name]
-        original_image = create_image(deck.key_image_format()['size'], button_config[key]['text'], style)
-        highlighted_image = create_highlighted_image(deck.key_image_format()['size'], button_config[key]['text'], style)
-        long_press_ack_image = create_image(deck.key_image_format()['size'], button_config[key]['text'], styles['long_press_ack'])
+        original_image = create_image(deck.key_image_format()['size'], button_config[key]['text'], style, font)
+        highlighted_image = create_highlighted_image(deck.key_image_format()['size'], button_config[key]['text'], style, font)
+        long_press_ack_image = create_image(deck.key_image_format()['size'], button_config[key]['text'], styles['long_press_ack'], font)
         images[key] = {
             'original': original_image,
             'highlighted': highlighted_image,
@@ -157,7 +163,7 @@ try:
             deck.set_key_image(key, images[key]['highlighted'])
         else:
             press_duration = time.time() - key_press_times[key]  # Calculate the press duration
-            if press_duration >= parameters['long_press_duration']:
+            if press_duration >= float(parameters['long_press_duration']):
                 print('Long press detected on key: {}'.format(key))
                 long_press_function = button_config[key].get('long_press')
                 if callable(long_press_function):
@@ -175,7 +181,7 @@ try:
         while not stop_flag.is_set():
             current_keys = list(key_press_times.keys())  # Create a list of current keys
             for key in current_keys:
-                if time.time() - key_press_times[key] >= parameters['long_press_duration']:
+                if time.time() - key_press_times[key] >= float(parameters['long_press_duration']):
                     deck.set_key_image(key, images[key]['long_press_ack'])
                     ack_action_function = button_config[key].get('ack_action')
                     if callable(ack_action_function):
